45140135637fb3820a56f6b2d04ee95b08109c5e
rohitkumardanda <rohitdanda@users.noreply.github.com>
2017-11-25 14:42:00 +0000

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -6,6 +6,6 @@ $formcontent="From: $name \n Message: $comments";
 $recipient = "rohitdanda@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+mail($recipient, $subject, $formcontent, $mailheader);
 header("Location : index.html");
 ?>
